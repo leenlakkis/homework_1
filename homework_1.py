93fb903df1558d0fa583e9e0fa4faa158d52075a
leenlakkis <leen.lakkis@gmail.com>
2024-01-24 00:25:10 +0100

    homework1

---
+++ homework_1.py
@@ -0,0 +1,22 @@
+print("Hello World")
+print("My Name is Leen")
+print(5+2)
+print(8/2)
+print(5%3)
+print(-8)
+print(float(-3))
+print(5<6)
+print(5>6)
+print(7|8)
+
+a=4
+print(a+3.0)
+a/=4
+print(a)
+b= 6
+print(a*b)
+b -=a
+print(b+2)
+c = a > b
+print(c and a)
+print(a+b+c)
